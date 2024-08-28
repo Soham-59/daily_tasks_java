f776710de02bc187483363613afc8074c7e78fd6
Soham_Deshmukh <sohamdeshmukh8391@gmail.com>
2024-08-28 18:29:39 +0530

    day3 completed

---
+++ Day-3/day3.java
@@ -0,0 +1,137 @@
+class codex{
+
+	public static void main(String args[]){
+
+	// day-3
+	
+	//1. print following pattern(* * * *)
+		int i;
+	for(i=1;i<=4;i++){
+
+		System.out.print("* ");
+
+			}
+	System.out.print("\n -------------------------------------------\n");
+
+	
+	//2. print following pattern 3 6 9 12
+	
+		System.out.print("print following pattern 3 6 9 12 ");
+		
+	for(i=1;i<=12;i++){
+
+		if(i%3==0){
+	
+			System.out.print(i+" ");
+		}
+			}
+	System.out.print("\n -------------------------------------------\n");
+
+	//3. print following pattern 0 1 0 1
+		
+	for(i=1;i<=12;i++){
+
+		if(i%3==0){
+	
+			System.out.print(i+" ");
+		}
+			}
+	System.out.print("\n -------------------------------------------\n");
+
+
+		for(i=1;i<=4;i++){
+	
+	if(i%2==1){
+
+		System.out.print(0+" ");
+
+			}
+	else{
+
+	System.out.print(1+" ");
+		
+		}
+
+			}
+
+	System.out.print("\n -------------------------------------------\n");
+
+	//4.print pattern A B C D
+
+		for(i=1;i<=4;i++){
+	
+	if(i==1){
+
+		System.out.print("A ");
+
+			}
+	else if(i==2){
+
+	System.out.print("B ");
+		
+		}
+
+			else if(i==3){
+
+				System.out.print("C ");
+		
+		}
+
+	else{
+
+		System.out.print("D ");
+				}
+
+
+			}
+
+	System.out.print("\n -------------------------------------------\n");
+	
+	
+	// 5 print pattern a b c d
+
+
+		for(i=1;i<=4;i++){
+	
+	if(i==1){
+
+		System.out.print("a ");
+
+			}
+	else if(i==2){
+
+	System.out.print("b ");
+		
+		}
+
+			else if(i==3){
+
+				System.out.print("c ");
+		
+		}
+
+	else{
+
+		System.out.print("d ");
+				}
+
+
+			}
+
+		
+
+
+
+
+
+
+
+	
+
+
+
+	}
+
+
+
+}
