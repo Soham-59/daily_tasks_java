95d36fcb8c1385c0784105328ecfec68f85bd504
Soham_Deshmukh <sohamdeshmukh8391@gmail.com>
2024-09-10 23:03:44 +0530

    Day 11 completed

---
+++ Day-11/codex.java
@@ -0,0 +1,60 @@
+class codex{
+
+	public static void main(String soham[]){
+
+	//1. print squares of first 10 numbers
+
+		for(int i=1;i<=10;i++){
+
+			System.out.println("The square of "+i+" is: "+i*i);
+	
+				}
+
+		System.out.print("\n----------------------------------------\n");
+
+	//2. print cubes of first 10 numbers
+
+		for(int i=1;i<=20;i++){
+
+			System.out.println("The cube of "+i+" is: "+i*i*i);
+	
+				}
+
+		System.out.print("\n----------------------------------------\n");
+
+	//3.print table of 2
+
+		int temp=2;
+		
+		for(int i=1;i<=10;i++){
+
+			System.out.print(temp*i+" ");
+	
+				}
+	System.out.print("\n----------------------------------------\n");
+
+		//4.print pattern
+
+		for(int i=1;i<=4;i++){
+
+			for(int j=1;j<=i;j++){
+
+				System.out.print("0\t");
+
+
+					}
+
+				System.out.println("\n");
+	
+			}
+
+
+
+
+
+
+		}
+
+
+
+	}
